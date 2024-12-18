476591eec2a2467f59e05b851afc0b376c533fc0
irfan2252738 <razairfan738@gmail.com>
2024-12-18 06:49:36 +0000

    changes

---
+++ 01_basic/04_string.js
@@ -0,0 +1,7 @@
+const myName = "Irfan"
+console.log(myName[0]);
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(1));
+console.log(myName.indexOf('f'));
+console.log(myName.charAt(1));
