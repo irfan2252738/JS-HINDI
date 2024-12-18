0b092e06f9a1667beb7a35fda0018d7d14f1de35
irfan2252738 <razairfan738@gmail.com>
2024-12-18 08:17:27 +0000

    numsandmaths

---
+++ 01_basic/05_nums_maths.js
@@ -0,0 +1,29 @@
+const numberOne = "Irfan"
+const numberTwo = new Number(200)
+//console.log(numberOne.toString().toUpperCase())
+//console.log(numberTwo.toFixed())
+
+const anotherNumber = 123.6
+//console.log(anotherNumber.toPrecision(3))
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+
+//++++++++++++++++++++Math++++++++++++++++++++++++++++
+
+//console.log(Math.abs(-4))
+//console.log(Math.round(5.6))
+//console.log(Math.ceil(6.9))
+//console.log(Math.floor(3.6))
+//console.log(Math.sqrt(36)) //square root of 6
+//console.log(Math.min(3, 6, 5, 9))
+//console.log(Math.max(3, 6, 5, 9))
+
+//console.log(Math.round(Math.random() * 10) + 1)
+
+const min = 5
+const max = 20
+
+console.log(Math.round(Math.random() * (10 + 1) + min))
+
