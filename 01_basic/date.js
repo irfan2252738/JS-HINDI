700a0ac4aba843cc8fdf371be4c15acde3cb0036
irfan2252738 <razairfan738@gmail.com>
2024-12-19 10:47:03 +0000

    date

---
+++ 01_basic/date.js
@@ -0,0 +1,14 @@
+//******Date is object
+const myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toLocaleString());
+
+const newCreatedDate = new Date("02-24-2024")
+//console.log(newCreatedDate.toLocaleString())
+
+const myDateStamp = Date.now()
+console.log(myDateStamp);
+console.log(Date.now())
+
+
+
